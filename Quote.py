ffdc7f15bba80e349476e1abf5f4aff08b5bddb2
Alejandro Munoz <amunoz239@yahoo.com>
2020-07-05 21:35:06 -0500

    Added Equation file, and also added user input for stocks

--- Quote.py
+++ Quote.py
@@ -1,9 +1,16 @@
-headers = { 'X-Finnhub-Token' : 'brv97snrh5r9k3fgt6b0'}
+from Equations import *
+headers = { 'X-Finnhub-Token' : 'brv97snrh5r9k3fgt6b0'} #API
 import requests
+import json
 
-r = requests.get('https://finnhub.io/api/v1/quote?symbol=AAPL',headers=headers)
-for i in r:
-    if i == 'c':
-        print (i)
 
-print("Using Json", r.json())
+inp = input('Please input a stock Ticker, if you want to quit please press 1 ')
+
+while inp != '1': #Will continue until users enters 1
+    ticker = 'https://finnhub.io/api/v1/quote?symbol='+ inp.upper()
+
+    data = requests.get(ticker,headers=headers)
+    print('From Data:\n',data.json())
+
+    inp = input('Please input another stock Ticker, if you want to quit please press 1 ')
+
